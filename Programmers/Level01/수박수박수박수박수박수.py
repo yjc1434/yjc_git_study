93793119888039e954cfe3f65c92b9985ed9477f
yjc1434 <yjc1434@gmail.com>
2022-04-16 23:56:26 +0900

    220416 프로그래머스 다시 풀기

---
+++ Programmers/Level01/수박수박수박수박수박수.py
@@ -0,0 +1,20 @@
+# 수박수박수박수박수박수?
+# https://programmers.co.kr/learn/courses/30/lessons/12922
+
+# 문제 설명
+# 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+# 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+# 제한 조건
+# n은 길이 10,000이하인 자연수입니다.
+
+def solution(n):
+    answer = ""
+    for i in range(n):
+        if i % 2 == 0:
+            answer += "수"
+        else:
+            answer += "박"
+    return answer
+
+print(solution(3))
